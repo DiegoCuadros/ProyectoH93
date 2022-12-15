73d32a8a49c7126ef247a4e7f5a50bb71ca88eb0
AlonsoCuadros <71740857@certus.edu.pe>
2022-12-14 19:42:08 -0500

    COMIT 2

--- H93CertusBackend-main/src/main/java/com/example/backendh93p1/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendh93p1.repository;
-
-import com.example.backendh93p1.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-}
--- H93CertusBackend-main/src/main/java/com/example/backendh93p1/repository/DatosClientesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendh93p1.repository;
-
-import com.example.backendh93p1.entity.DatosClientesEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DatosClientesRepository extends CrudRepository<DatosClientesEntity,Integer> {
-}
